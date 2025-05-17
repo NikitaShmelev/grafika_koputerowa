00abde7879ebe5cead0d2ee8dc7101c4feed3324
nshmelyov <shmelev507@gmail.com>
2025-05-17 09:37:50 +0200

    zad2 update

--- zad2/main.cpp
+++ zad2/main.cpp
@@ -5,10 +5,18 @@
 #include <glm/gtc/type_ptr.hpp>
 #include <iostream>
 
+// Program wyświetla 2 trójkąty i 2 prostokąty.
+// Każda figura ma inny kolor i jest animowana.
+// Figury nie wychodzą poza obszar okna dzięki detekcji kolizji i odbiciom (dla Figury 1 i 4).
+
+// Wymiary okna
+const unsigned int SCR_WIDTH  = 800;
+const unsigned int SCR_HEIGHT = 600;
+
 // Vertex shader
 const char* vertexShaderSrc = R"(#version 330 core
 layout(location = 0) in vec3 aPos;
-uniform mat4 uTransform;
+uniform mat4 uTransform;  // Transformacja modeli
 void main() {
     gl_Position = uTransform * vec4(aPos, 1.0);
 }
@@ -17,33 +25,27 @@ void main() {
 // Fragment shader
 const char* fragmentShaderSrc = R"(#version 330 core
 out vec4 FragColor;
-uniform vec4 uColor;
-void main() {
-    FragColor = uColor;
-}
+uniform vec4 uColor;  // Kolor figury
+void main() { FragColor = uColor; }
 )";
 
-// Window size
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
-
-// Utility: compile shader and check errors
+// Kompilacja shadera i sprawdzanie błędów
 unsigned int compileShader(unsigned int type, const char* src) {
-    unsigned int sh = glCreateShader(type);
-    glShaderSource(sh, 1, &src, nullptr);
-    glCompileShader(sh);
+    unsigned int shader = glCreateShader(type);
+    glShaderSource(shader, 1, &src, nullptr);
+    glCompileShader(shader);
     int success;
-    glGetShaderiv(sh, GL_COMPILE_STATUS, &success);
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
     if (!success) {
-        char log[512];
-        glGetShaderInfoLog(sh, 512, nullptr, log);
-        std::cerr << "Shader compile error: " << log << std::endl;
+        char buf[512];
+        glGetShaderInfoLog(shader, 512, nullptr, buf);
+        std::cerr << "Shader compile error: " << buf << std::endl;
     }
-    return sh;
+    return shader;
 }
 
 int main() {
-    // Init GLFW
+    // Inicjalizacja GLFW
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -51,122 +53,160 @@ int main() {
 
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Animated Shapes", nullptr, nullptr);
     if (!window) {
-        std::cerr << "Failed to create GLFW window" << std::endl;
+        std::cerr << "Nie można utworzyć okna" << std::endl;
         glfwTerminate();
         return -1;
     }
     glfwMakeContextCurrent(window);
 
-    // Load GLAD
+    // Wczytanie funkcji OpenGL
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cerr << "Failed to initialize GLAD" << std::endl;
+        std::cerr << "Nie udało się zainicjalizować GLAD" << std::endl;
         return -1;
     }
     glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
 
-    // Build shader program
+    // Budowa programu shaderów
     unsigned int vs = compileShader(GL_VERTEX_SHADER, vertexShaderSrc);
     unsigned int fs = compileShader(GL_FRAGMENT_SHADER, fragmentShaderSrc);
-    unsigned int shaderProg = glCreateProgram();
-    glAttachShader(shaderProg, vs); glAttachShader(shaderProg, fs);
-    glLinkProgram(shaderProg);
-    glDeleteShader(vs); glDeleteShader(fs);
+    unsigned int prog = glCreateProgram();
+    glAttachShader(prog, vs);
+    glAttachShader(prog, fs);
+    glLinkProgram(prog);
+    glDeleteShader(vs);
+    glDeleteShader(fs);
 
-    // Define geometry
+    // Dane geometrii: trójkąt i prostokąt
     float triVertices[] = {
-         0.0f,  0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f
+        0.0f,  0.5f, 0.0f,
+       -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f
     };
     float rectVertices[] = {
-        -0.5f,  0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.5f,  0.5f, 0.0f
+       -0.5f,  0.5f, 0.0f,
+       -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.5f,  0.5f, 0.0f
     };
     unsigned int rectIndices[] = { 0,1,2, 0,2,3 };
 
-    // VAO/VBO for triangle
+    // VAO/VBO dla trójkąta
     unsigned int VAO_tri, VBO_tri;
     glGenVertexArrays(1, &VAO_tri);
     glGenBuffers(1, &VBO_tri);
     glBindVertexArray(VAO_tri);
     glBindBuffer(GL_ARRAY_BUFFER, VBO_tri);
     glBufferData(GL_ARRAY_BUFFER, sizeof(triVertices), triVertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    // VAO/VBO/EBO for rectangle
-    unsigned int VAO_rect, VBO_rect, EBO_rect;
-    glGenVertexArrays(1, &VAO_rect);
-    glGenBuffers(1, &VBO_rect);
-    glGenBuffers(1, &EBO_rect);
-    glBindVertexArray(VAO_rect);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO_rect);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(rectVertices), rectVertices, GL_STATIC_DRAW);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO_rect);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(rectIndices), rectIndices, GL_STATIC_DRAW);
     glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(float),(void*)0);
     glEnableVertexAttribArray(0);
 
-    // Get uniform locations
-    glUseProgram(shaderProg);
-    int uniTrans = glGetUniformLocation(shaderProg, "uTransform");
-    int uniColor = glGetUniformLocation(shaderProg, "uColor");
+    // VAO/VBO/EBO dla prostokąta
+    unsigned int VAO_rect, VBO_rect, EBO_rect;
+    glGenVertexArrays(1,&VAO_rect);
+    glGenBuffers(1,&VBO_rect);
+    glGenBuffers(1,&EBO_rect);
+    glBindVertexArray(VAO_rect);
+    glBindBuffer(GL_ARRAY_BUFFER,VBO_rect);
+    glBufferData(GL_ARRAY_BUFFER,sizeof(rectVertices),rectVertices,GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO_rect);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(rectIndices),rectIndices,GL_STATIC_DRAW);
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(float),(void*)0);
+    glEnableVertexAttribArray(0);
 
-    // Render loop
+    // Lokalizacje uniformów
+    glUseProgram(prog);
+    int locTrans = glGetUniformLocation(prog, "uTransform");
+    int locColor = glGetUniformLocation(prog, "uColor");
+
+    // Parametry animacji i początkowe pozycje
+    const float halfTri = 0.5f * 0.6f; // pół rozmiaru trójkąta w skali
+    const float halfRec = 0.5f * (0.4f + 0.1f);
+    
+    // --- Ruch Figury 1: trójkąt z odbiciami ---
+    static glm::vec2 pos1(-0.6f, 0.6f);
+    static glm::vec2 vel1 = glm::normalize(glm::vec2(1.0f,0.5f)) * 0.5f;
+
+    // --- Ruch Figury 4: prostokąt z łączoną animacją ---
+    static glm::vec2 pos4( 0.6f,-0.6f);
+    static glm::vec2 vel4 = glm::normalize(glm::vec2(-1.0f,0.3f)) * 0.4f;
+
+    // Pętla główna
     while (!glfwWindowShouldClose(window)) {
-        float t = glfwGetTime();
+        float t = (float)glfwGetTime();
         glClearColor(0.1f,0.1f,0.1f,1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-        glUseProgram(shaderProg);
+        glUseProgram(prog);
 
-        // Shape1: triangle, straight line (horizontal sine)
-        glm::mat4 trans1 = glm::translate(glm::mat4(1.0f), glm::vec3(sin(t)*0.5f, 0.6f, 0.0f));
-        glUniformMatrix4fv(uniTrans,1,GL_FALSE,glm::value_ptr(trans1));
-        glUniform4f(uniColor,1.0f,0.0f,0.0f,1.0f);
-        glBindVertexArray(VAO_tri);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // --- Figura 1 ---
+        // Trójkąt: ruch prostoliniowy z odbiciami od krawędzi okna
+        {
+            float dt = 0.01f;
+            pos1 += vel1 * dt;
+            // Odbicie w poziomie
+            if (pos1.x + halfTri > 1.0f || pos1.x - halfTri < -1.0f) vel1.x *= -1;
+            // Odbicie w pionie
+            if (pos1.y + halfTri > 1.0f || pos1.y - halfTri < -1.0f) vel1.y *= -1;
+            glm::mat4 M = glm::translate(glm::mat4(1.0f), glm::vec3(pos1,0.0f));
+            M = glm::scale(M, glm::vec3(0.6f));
+            glUniformMatrix4fv(locTrans,1,GL_FALSE,glm::value_ptr(M));
+            glUniform4f(locColor,1.0f,0.0f,0.0f,1.0f);
+            glBindVertexArray(VAO_tri);
+            glDrawArrays(GL_TRIANGLES,0,3);
+        }
 
-        // Shape2: triangle, rotation
-        glm::mat4 trans2 = glm::translate(glm::mat4(1.0f), glm::vec3(-0.6f, -0.6f,0.0f));
-        trans2 = glm::rotate(trans2, t, glm::vec3(0.0f,0.0f,1.0f));
-        glUniformMatrix4fv(uniTrans,1,GL_FALSE,glm::value_ptr(trans2));
-        glUniform4f(uniColor,0.0f,1.0f,0.0f,1.0f);
-        glBindVertexArray(VAO_tri);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // --- Figura 2 ---
+        // Trójkąt: stała rotacja w miejscu (górny prawy)
+        {
+            glm::mat4 M = glm::translate(glm::mat4(1.0f), glm::vec3(0.6f,0.6f,0.0f));
+            M = glm::rotate(M, t, glm::vec3(0,0,1));
+            M = glm::scale(M, glm::vec3(0.6f));
+            glUniformMatrix4fv(locTrans,1,GL_FALSE,glm::value_ptr(M));
+            glUniform4f(locColor,0.0f,1.0f,0.0f,1.0f);
+            glBindVertexArray(VAO_tri);
+            glDrawArrays(GL_TRIANGLES,0,3);
+        }
 
-        // Shape3: rectangle, scale pulsate
-        float scale = (sin(t*2.0f)*0.5f + 1.0f) * 0.25f + 0.25f;
-        glm::mat4 trans3 = glm::translate(glm::mat4(1.0f), glm::vec3(0.6f, 0.6f,0.0f));
-        trans3 = glm::scale(trans3, glm::vec3(scale));
-        glUniformMatrix4fv(uniTrans,1,GL_FALSE,glm::value_ptr(trans3));
-        glUniform4f(uniColor,0.0f,0.0f,1.0f,1.0f);
-        glBindVertexArray(VAO_rect);
-        glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,0);
+        // --- Figura 3 ---
+        // Prostokąt: pulsacyjne powiększanie i zmniejszanie (dolny lewy)
+        {
+            float s = 0.4f + sin(t*2.0f)*0.1f;
+            glm::mat4 M = glm::translate(glm::mat4(1.0f), glm::vec3(-0.6f,-0.6f,0.0f));
+            M = glm::scale(M, glm::vec3(s));
+            glUniformMatrix4fv(locTrans,1,GL_FALSE,glm::value_ptr(M));
+            glUniform4f(locColor,0.0f,0.0f,1.0f,1.0f);
+            glBindVertexArray(VAO_rect);
+            glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,0);
+        }
 
-        // Shape4: rectangle, combine (translate+rotate+scale)
-        glm::mat4 trans4 = glm::translate(glm::mat4(1.0f), glm::vec3(sin(t)*0.4f, cos(t)*0.4f,0.0f));
-        trans4 = glm::rotate(trans4, t, glm::vec3(0.0f,0.0f,1.0f));
-        float scl = (cos(t)*0.5f + 1.0f) * 0.25f + 0.25f;
-        trans4 = glm::scale(trans4, glm::vec3(scl));
-        glUniformMatrix4fv(uniTrans,1,GL_FALSE,glm::value_ptr(trans4));
-        glUniform4f(uniColor,1.0f,1.0f,0.0f,1.0f);
-        glBindVertexArray(VAO_rect);
-        glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,0);
+        // --- Figura 4 ---
+        // Prostokąt: łączona animacja ruchu z odbiciami, rotacji i skali
+        {
+            float dt = 0.01f;
+            pos4 += vel4 * dt;
+            if (pos4.x + halfRec > 1.0f || pos4.x - halfRec < -1.0f) vel4.x *= -1;
+            if (pos4.y + halfRec > 1.0f || pos4.y - halfRec < -1.0f) vel4.y *= -1;
+            float sc = 0.4f + cos(t)*0.1f;
+            glm::mat4 M = glm::translate(glm::mat4(1.0f), glm::vec3(pos4,0.0f));
+            M = glm::rotate(M, t, glm::vec3(0,0,1));
+            M = glm::scale(M, glm::vec3(sc));
+            glUniformMatrix4fv(locTrans,1,GL_FALSE,glm::value_ptr(M));
+            glUniform4f(locColor,1.0f,1.0f,0.0f,1.0f);
+            glBindVertexArray(VAO_rect);
+            glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,0);
+        }
 
-        // Swap
+        // Zamiana buforów i przetwarzanie zdarzeń
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    // Cleanup
+    // Sprzątanie
     glDeleteVertexArrays(1,&VAO_tri);
     glDeleteBuffers(1,&VBO_tri);
     glDeleteVertexArrays(1,&VAO_rect);
     glDeleteBuffers(1,&VBO_rect);
     glDeleteBuffers(1,&EBO_rect);
-    glDeleteProgram(shaderProg);
+    glDeleteProgram(prog);
     glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
